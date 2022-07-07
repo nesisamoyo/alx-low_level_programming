65bb4bca36009f63db61ddcc37869e598df42194
Nesisa Moyo <moyonesisam@gmail.com>
2022-07-07 10:56:04 +0200

    Update 0-putchar.c
    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,26 +10,16 @@
 
 int main(void)
 
-{
+{	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	char abe[] = "_putchar";
-
-
-
-	int c;
-
-
-
-	for (c = 0; c < 8; c++)
-
-	{
-
-		_putchar(abe[c]);
-
-	}
-
-	_putchar('\n');
-
-	return (0);
+		return (0);
 
 }
