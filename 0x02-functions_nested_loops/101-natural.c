bfb40ec21697d52eeab8c0e7dbd8a588de8fa000
Nesisa Moyo <moyonesisam@gmail.com>
2022-07-15 09:12:58 +0200

    Update 101-natural.c
    task 12

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - sums 3-5_cous - Entry point
+ * main - Entry point
  *
- * Return:  (0)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i = 0;
-	int m3 = 3;
-	int m5 = 5;
-	int sum = 0;
-	int lim = 1024;
+	int number, sum = 0;
 
-	while (i == 0)
+	for (number = 0; number < 1024; number++)
 	{
-
-		if (m3 < lim)
-		{
-			sum += m3;
-			m3 += 3;
-		}
-
-		if (m5 < lim && (m5 % 3) != 0)
-		{
-			sum += m5;
-			m5 += 5;
-		}
-		else if (m5 % 3 == 0)
-		{
-			m5 += 5;
-		}
-
-		if (m3 >= lim && m5 >= lim)
-		{
-			i = 1;
-		}
+		if ((number % 5) == 0 || (number % 3) == 0)
+			sum += number;
 	}
-	printf("%d\n", sum);
+	printf("%d", sum);
+	printf("\n");
+
 	return (0);
 }
