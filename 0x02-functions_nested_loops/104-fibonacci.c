23d5cf6442019a063695bf1fc6efdfa4ce439966
Nesisa Moyo <moyonesisam@gmail.com>
2022-07-15 09:35:34 +0200

    Update 104-fibonacci.c
    task 14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
- *
- * Purpose - no hardcode
- *
- * Return:  (Success)
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
  */
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	int i;
+	unsigned long first = 1;
+	unsigned long second = 2;
+	unsigned long long int sum_of_two = 3;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
+	printf("1, 2, ");
+	for (i = 3; i <= 98; i++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
+		sum_of_two = first + second;
+		printf("%llu", sum_of_two);
+		first = second;
+		second = sum_of_two;
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		if (i < 98)
+			printf(", ");
 	}
 	printf("\n");
+
 	return (0);
 }
