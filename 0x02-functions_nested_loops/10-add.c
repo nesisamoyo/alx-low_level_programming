1b81d26fe6e9247268f6ed99fc2c7ee74af405c5
Nesisa Moyo <moyonesisam@gmail.com>
2022-07-15 09:10:53 +0200

    Update 10-add.c
    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
-#include "holberton.h"
-/**
- * add - adds two integers and returns the result
- * @a: an integer to add to b
- * @b: an integer to add to a
- * Return: Void.
- */
+#include "main.h"
 
+/**
+ * add - check description
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers and returns the result
+ * Return: Result = sum of a and b
+ */
 int add(int a, int b)
 {
 	return (a + b);
 }
-
